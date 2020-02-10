70c2816d9f13151c3bc8c3d3d74db269dc47102e
lili <lili@chainup.com>
2020-02-10 12:41:20 +0800

    注解task

--- src/main/java/org/lili/task/FetchSomethingTask.java
+++ src/main/java/org/lili/task/FetchSomethingTask.java
@@ -10,13 +10,14 @@ package org.lili.task;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @EnableScheduling
+@Service
 public class FetchSomethingTask {
-
 
     @Scheduled(cron = "*/5 * * * * ?")
     public void batchFetchMarketNetValue() {
